d24af1dc3a12d144d452be985468b6bb0d1528e2
Raja Poudel <raja.poudel@prosent.ai>
2024-08-16 14:07:05 +0700

    Build: added the console.log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+  console.log('Added the console.log')
   res.json({ message: 'Welcome to my express app.' })
 })
 
